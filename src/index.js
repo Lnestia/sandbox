c16ce8ac15daaee6ced465d32be1cc14a28be927
Lnestia <62011748+Lnestia@users.noreply.github.com>
2020-12-18 13:27:47 +0900

    d d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-//```````````
+import "./styles.css";
 
-const flag1 = true;
-const flag2 = true;
-if (flag1 || flag2) {
-  console.log("1or2 true");
-}
+const onClickAdd = () => {
+  //text-boxの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-if (flag1 && flag2) {
-  console.log("1or2 true");
-}
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // li生成
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "complete";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+
+  //div 要素追加
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了に追加する
+  document.getElementById("incomplate-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
